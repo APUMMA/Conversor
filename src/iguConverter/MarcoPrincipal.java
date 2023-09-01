5363b6f23aee7961e45cbd968d6d09df8ca71861
APUMA <angeldelapuentem@gmail.com>
2023-09-01 17:19:03 -0500

    Implementación lógica cambio monedas

--- src/iguConverter/MarcoPrincipal.java
+++ src/iguConverter/MarcoPrincipal.java
@@ -1,10 +1,10 @@
 package iguConverter;
 
-
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -23,7 +23,7 @@ import javax.swing.JTextField;
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% <<<<<CREANDO LA INTERFAZ GRAFICA>>>>> %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 public class MarcoPrincipal extends JFrame {
 
-  // Declaración de variables  de la IGU.
+  // Declaración de variables  de la Interfaz Gráfica de Usuario (IGU).
   private JTabbedPane contenedorPestanias;
   private JPanel pestania1, pestania2;
   private JLabel lblImporte, lbl_De, lbl_a, lblImporte2, lblResultado, lblMonedaBase, lblMonedaFinal;
@@ -74,17 +74,17 @@ public class MarcoPrincipal extends JFrame {
     lblImporte = new JLabel("Importe:");
     lbl_De = new JLabel("De:");
     lbl_a = new JLabel("a:");
-    // TODO
-    lblImporte2 = new JLabel("250");
-    lblResultado = new JLabel("250");
-    lblMonedaBase = new JLabel("250");
-    lblMonedaFinal = new JLabel("250");
+   
+    lblImporte2 = new JLabel("000000");
+    lblResultado = new JLabel("00000");
+    lblMonedaBase = new JLabel("00000");
+    lblMonedaFinal = new JLabel("00000");
 
     lblImporte.setBounds(20, 20, 80, 40);
     lbl_De.setBounds(270, 20, 80, 40);
     lbl_a.setBounds(520, 20, 80, 40);
-    lblImporte2.setBounds(20, 120, 80, 40);
-    lblResultado.setBounds(20, 160, 230, 50);
+    lblImporte2.setBounds(20, 120, 280, 40);
+    lblResultado.setBounds(20, 160, 430, 50);
     lblMonedaBase.setBounds(20, 210, 230, 40);
     lblMonedaFinal.setBounds(20, 240, 230, 40);
 
@@ -92,6 +92,8 @@ public class MarcoPrincipal extends JFrame {
     lbl_De.setForeground(Color.BLACK);
     lbl_a.setForeground(Color.BLACK);
     // lblImporte2.setForeground(Color.BLACK);
+    lblMonedaBase.setForeground(Color .GRAY);
+    lblMonedaFinal.setForeground(Color .GRAY);
     lblResultado.setForeground(Color.BLUE);
     lblResultado.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
     lblResultado.setFont(new Font("Tahoma", 2, 42));
@@ -102,10 +104,10 @@ public class MarcoPrincipal extends JFrame {
     txtImporte.setBounds(20, 55, 230, 30);
 
     // --------------------------------------------- <ComboBox> ---------------------------------------------
-    cmbMonedaBase = new JComboBox<String>(new String[] { "USD - Dólar estadounidense", "COP - Peso colombiano",
+    cmbMonedaBase = new JComboBox<>(new String[] { "USD - Dólar estadounidense", "COP - Peso colombiano",
                                                           "EUR - Euro", "GBP - Libra esterlína",
                                                           "JPY - Yen japonés", "BRL - Real brasilero" });
-    cmbMonedaFinal = new JComboBox<String>(new String[] { "EUR - Euro", "USD - Dólar estadounidense", 
+    cmbMonedaFinal = new JComboBox<>(new String[] { "EUR - Euro", "USD - Dólar estadounidense", 
                                                           "COP - Peso colombiano", "GBP - Libra esterlína", 
                                                           "JPY - Yen japonés", "BRL - Real brasilero" });
 
@@ -139,72 +141,170 @@ public class MarcoPrincipal extends JFrame {
   }  
 
     // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% <<<<<EVENTOS DEL FORMULARIO>>>>> %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    // Declaraciòn de variables de eventos.
+    double importe = 0;
     double resultado = 0;
-    String de = "EUR";
-    String a = "USD";
-  
+    int a = 0;
+    String de = "USD - Dólar estadounidense";
+
     private void configurarEventosFormulario() {
+
+      txtImporte .addActionListener(new ActionListener() {
+        public void actionPerformed (ActionEvent event) {
+
+          convertir();
+
+        }
+      });
   
       cmbMonedaBase .addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent event) {
-          de = cmbMonedaBase.getSelectedItem().toString();
-          
+          de = (String) cmbMonedaBase.getSelectedItem();
+          // monedaBase = cmbMonedaBase.getSelectedItem().toString();
+          convertir();
         }
       });
   
       cmbMonedaFinal .addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent event) {
-          a = cmbMonedaFinal.getSelectedItem().toString();
-          
+          a = cmbMonedaFinal.getSelectedIndex();
+          convertir();
         }
       });
   
+      btnConvertir .addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+
+          convertir();
+          
+        }
+      });
+
       btnLimpiar .addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent event) {
           limpiarFormulario();
         }
       });
   
-      btnConvertir .addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent event) {
-          convertir();
-        }
-      });
-  
-    }
-  
-    // --------------------------------- <<<<<Metodo limpiar formulario>>>>> ----------------------------------
-    private void limpiarFormulario() {
-      txtImporte.setText("");
-      cmbMonedaBase.setSelectedIndex(0);
-      cmbMonedaFinal.setSelectedIndex(0);
-      lblImporte2.setText(" ");
-      lblResultado.setText(" ");
-      lblMonedaBase.setText(" ");
-      lblMonedaFinal.setText(" ");
-  
     }
   
     // -------------------------------------- <<<<<Metodo convertir>>>>> --------------------------------------
-    int importe;
-  
     private void convertir() {
       // Validando los datos ingresados en el JTextField.
       if (txtImporte.getText().isBlank() || !validarNumero(txtImporte.getText().trim())) {
   
         JOptionPane.showMessageDialog(this, "El campo importe debe ser numérico");
-  
-      }else{
-        importe = Integer.parseInt(txtImporte.getText());
-        lblImporte2.setText(" " + importe +" =");
-        // TODO
-        lblResultado.setText(" " + importe);
-        lblMonedaBase.setText(" " + importe +" ="); 
-        lblMonedaFinal.setText(" " + importe +" =");
-  
-        JOptionPane.showMessageDialog(this, "Conversión realizada");
-  
-      }
+        }
+        // Declaración de variables.
+        importe = Double.parseDouble(txtImporte.getText());
+        String de2 = de.substring(5);
+        
+        switch (de) {
+          case "USD - Dólar estadounidense":
+              if (a == 0) {
+                resultado = (importe * 0.92);
+              }else if (a == 1) {
+                resultado = (importe * 1);
+              }else if (a == 2) {
+                resultado = (importe * 4098.53);
+              }else if (a == 3) {
+                resultado = (importe * 0.79);
+              }else if (a == 4) {
+                resultado = (importe * 145.93);
+              }else if (a == 5) {
+                resultado = (importe * 4.89);
+              }
+              break;
+          case "COP - Peso colombiano":
+              if (a == 0) {
+                resultado = (importe * 0.00022);
+              }else if (a == 1) {
+                resultado = (importe * 0.00025);
+              }else if (a == 2) {
+                resultado = (importe * 1);
+              }else if (a == 3) {
+                resultado = (importe * 0.00019);
+              }else if (a == 4) {
+                resultado = (importe * 0.036);
+              }else if (a == 5) {
+                resultado = (importe * 0.0012);
+              }
+              break;
+          case "EUR - Euro":
+              if (a == 0) {
+                resultado = (importe * 1);
+              }else if (a == 1) {
+                resultado = (importe * 1.09);
+              }else if (a == 2) {
+                resultado = (importe * 4479.32);
+              }else if (a == 3) {
+                resultado = (importe * 0.86);
+              }else if (a == 4) {
+                resultado = (importe * 159.65);
+              }else if (a == 5) {
+                resultado = (importe * 5.34);
+              }
+              break;
+          case "GBP - Libra esterlína":
+              if (a == 0) {
+                resultado = (importe * 1.16);
+              }else if (a == 1) {
+                resultado = (importe * 1.27);
+              }else if (a == 2) {
+                resultado = (importe * 5210.26);
+              }else if (a == 3) {
+                resultado = (importe *1);
+              }else if (a == 4) {
+                resultado = (importe * 185.80);
+              }else if (a == 5) {
+                resultado = (importe * 6.22);
+              }
+              break;
+          case "JPY - Yen japonés":
+              if (a == 0) {
+                resultado = (importe * 0.0063);
+              }else if (a == 1) {
+                resultado = (importe * 0.0068);
+              }else if (a == 2) {
+                resultado = (importe * 28.05);
+              }else if (a == 3) {
+                resultado = (importe * 0.0054);
+              }else if (a == 4) {
+                resultado = (importe * 1);
+              }else if (a == 5) {
+                resultado = (importe * 0.033);
+              }
+              break;
+          case "BRL - Real brasilero":
+              if (a == 0) {
+                resultado = (importe * 0.19);
+              }else if (a == 1) {
+                resultado = (importe * 0.20);
+              }else if (a == 2) {
+                resultado = (importe * 838.15);
+              }else if (a == 3) {
+                resultado = (importe * 0.16);
+              }else if (a == 4) {
+                resultado = (importe * 29.87);
+              }else if (a == 5) {
+                resultado = (importe * 1);
+              }
+              break;
+        }
+
+        DecimalFormat formatter = new DecimalFormat("#.#####");
+
+        String a2 = "EUR - Euro"; // Declaración de variable a2.
+        for (int a = 0; a < 5; a++) {
+          a2 = (String) cmbMonedaFinal.getSelectedItem();
+        }
+        lblImporte2.setText(txtImporte.getText() + " " + de2 + " ="); 
+        lblResultado.setText(String.valueOf(formatter.format(resultado)) + " " + a2.substring(0, 4));
+
+        lblMonedaBase.setText("1 " + de.substring(0, 4) + " ="); 
+        lblMonedaFinal.setText("1 " + a2.substring(0, 4) + " =");
+           
       
     }
   
@@ -218,8 +318,14 @@ public class MarcoPrincipal extends JFrame {
   
     }
   
-    public String aMoneda(double importe){
-      return null;
-    }
+    // --------------------------------- <<<<<Metodo limpiar formulario>>>>> ----------------------------------
+    private void limpiarFormulario() {
+      txtImporte.setText(" ");
+      lblImporte2.setText(" ");
+      lblResultado.setText(" ");
+      lblMonedaBase.setText(" ");
+      lblMonedaFinal.setText(" ");
   
+    }
+
 }
